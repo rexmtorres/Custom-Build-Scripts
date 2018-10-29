2fdcd882d71e9defb040ba8d29ea3f717ef42989
Rex M. Torres <rexmtorres@gmail.com>
2018-10-30 02:22:47 +0800

    Plugin changes

--- plugin/DeliveryHelper/deliveryHelperPlugin/src/main/groovy/com/rexmtorres/deliveryhelper/DeliveryPlugin.groovy
+++ plugin/DeliveryHelper/deliveryHelperPlugin/src/main/groovy/com/rexmtorres/deliveryhelper/DeliveryPlugin.groovy
@@ -1,13 +1,17 @@
 package com.rexmtorres.deliveryhelper
 
+import com.android.build.gradle.api.ApplicationVariant
 import com.android.build.gradle.api.BaseVariant
-import org.gradle.api.NamedDomainObjectContainer
+import com.android.build.gradle.api.LibraryVariant
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.external.javadoc.JavadocMemberLevel
 
 class JavaDocSettings {
+    BaseVariant variant
+
     String title
     JavadocMemberLevel javadocMemberLevel
     ConfigurableFileCollection additionalSourceFiles
@@ -17,106 +21,146 @@ class JavaDocSettings {
 
     @Override
     String toString() {
-        return title
+        return """|doc${variant.name.capitalize()} {
+                  |    variant = $variant
+                  |    title = $title
+                  |    javadocMemberLevel = $javadocMemberLevel
+                  |    additionalSourceFiles = $additionalSourceFiles
+                  |    additionalClasspathFiles = $additionalClasspathFiles
+                  |    excludedFiles = $excludedFiles
+                  |    outputFile = $outputFile
+                  |}""".stripMargin()
     }
 }
 
-class Delivery {
-    static Project project
-
-    String name
-
-    BaseVariant variant
-    File outputFile
+class ApplicationDelivery {
+    ApplicationVariant variant
+    File apkFile
     File unsignedApkFile
     File proguardMapDir
 
-    private JavaDocSettings javaDocSettings
-
-    Delivery(final String name) {
-        this.name = name
+    @Override
+    String toString() {
+        return """|app${variant.name.capitalize()} {
+                  |    variant = $variant
+                  |    apkFile = $apkFile
+                  |    unsignedApkFile = $unsignedApkFile
+                  |    proguardMapDir = $proguardMapDir
+                  |}""".stripMargin()
     }
+}
 
-    void javaDoc(final Closure javaDocClosure) {
-        javaDocSettings = new JavaDocSettings()
+class LibraryDelivery {
+    LibraryVariant variant
+    File aarFile
+    File jarFile
+    File proguardMapDir
 
-        project.configure(javaDocSettings, javaDocClosure)
-        if (javaDocClosure.hasProperty("title")) {
-            println "  title: " + javaDocClosure.getProperty("title")
+    @Override
+    String toString() {
+        return """|lib${variant.name.capitalize()} {
+                  |    variant = $variant
+                  |    aarFile = $aarFile
+                  |    jarFile = $jarFile
+                  |    proguardMapDir = $proguardMapDir
+                  |}""".stripMargin()
+    }
+}
+
+class DeliveryExtension {
+    static Project project
+
+    private BaseVariant[] variants = []
+
+    private ApplicationDelivery[] appDeliveries = []
+    private LibraryDelivery[] libDeliveries = []
+    private JavaDocSettings[] javaDocSettings = []
+
+    void app(final Closure appClosure) {
+        def app = new ApplicationDelivery()
+        project.configure(app, appClosure)
+
+        if (app.variant == null) {
+            throw new GradleException("app.variant cannot be null!")
         }
 
+        if (app.apkFile == null) {
+            throw new GradleException("app.apkFile cannot be null!")
+        }
+
+        appDeliveries += app
+
+        if (!variants.contains(app.variant)) {
+            variants += app.variant
+        }
+    }
+
+    void lib(final Closure libClosure) {
+        def lib = new LibraryDelivery()
+        project.configure(lib, libClosure)
+
+        if (lib.variant == null) {
+            throw new GradleException("lib.variant cannot be null!")
+        }
+
+        if (lib.aarFile == null) {
+            throw new GradleException("lib.aarFile cannot be null!")
+        }
+
+        libDeliveries += lib
+
+        if (!variants.contains(lib.variant)) {
+            variants += lib.variant
+        }
+    }
+
+    void javadoc(final Closure javadocClosure) {
+        def javadoc = new JavaDocSettings()
+        project.configure(javadoc, javadocClosure)
+
+        if (javadoc.variant == null) {
+            throw new GradleException("javadoc.variant cannot be null!")
+        }
+
+        if (javadoc.outputFile == null) {
+            throw new GradleException("javadoc.outputFile cannot be null!")
+        }
+
+        javaDocSettings += javadoc
+
+        if (!variants.contains(lib.variant)) {
+            variants += lib.variant
+        }
+    }
+
+    @Override
+    String toString() {
+        appDeliveries.each {
+            println "app: $it"
+        }
+        libDeliveries.each {
+            println "lib: $it"
+        }
+        javaDocSettings.each {
+            println "doc: $it"
+        }
+        return super.toString()
     }
 }
 
 class DeliveryPlugin implements Plugin<Project> {
     private def resVersion = "v1"
     private def binZip = "/res/bin_${resVersion}.zip"
-    private def cacheLoc = "${System.getProperty("user.home")}/.gradle/rmtcache/com.rexmtorres.deliveryhelper/${resVersion}"
+    private
+    def cacheLoc = "${System.getProperty("user.home")}/.gradle/rmtcache/com.rexmtorres.deliveryhelper/${resVersion}"
 
     void apply(Project project) {
-        Delivery.project = project
+        DeliveryExtension.project = project
 
-        NamedDomainObjectContainer<Delivery> deliveryContainer = project.container(Delivery)
+        def delivery = project.extensions.create("delivery", DeliveryExtension)
 
-        project.extensions.add("deliveries", deliveryContainer)
-
-        project.task('hello') {
+        project.task("createDelivery") {
             doLast {
-                def deliveries = project.extensions.getByName("deliveries")
-
-                deliveries.all { del ->
-                    println "${del.name} {"
-
-                    if (del.variant != null) {
-                        println "    variant = ${del.variant.name}"
-                    }
-
-                    if (del.outputFile != null) {
-                        println "    outputFile = ${del.outputFile}"
-                    }
-
-                    if (del.unsignedApkFile != null) {
-                        println "    unsignedApkFile = ${del.unsignedApkFile}"
-                    }
-
-                    if (del.proguardMapDir != null) {
-                        println "    proguardMapDir = ${del.proguardMapDir}"
-                    }
-
-                    if (del.javaDocSettings != null) {
-                        println "    javaDoc = ${del.javaDocSettings}"
-                    }
-
-                    println "}"
-                }
-
-//                println 'Hello from GreetingPlugin'
-//
-//                def resUri = getClass().getResource(binZip)
-//                println "URL: $resUri"
-//
-//                def cacheRoot = new File("${cacheLoc}")
-//
-//                if (!cacheRoot.exists()) {
-//                    cacheRoot.mkdirs()
-//                }
-//
-//                def cachedBin = new File(cacheRoot, "bin.zip")
-//
-//                if (!cachedBin.exists()) {
-//                    println "$cachedBin does not exist."
-//
-//                    resUri.withInputStream {
-//                        cachedBin << it
-//                    }
-//                } else {
-//                    println "$cachedBin already exists."
-//                }
-//
-//                project.copy {
-//                    from project.zipTree { cachedBin }
-//                    into cacheRoot
-//                }
             }
         }
     }
